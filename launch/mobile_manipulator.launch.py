f19ad85a19b3fdf989980dad1186c02f8b87982e
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2024-09-30 15:08:30 +0000

    refactor_launch_files
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- launch/mobile_manipulator.launch.py
+++ launch/mobile_manipulator.launch.py
@@ -5,7 +5,6 @@
 from launch.actions import IncludeLaunchDescription
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
 from rcdt_utilities.launch_utils import get_file_path, get_robot_description
 
 # Get robot_description:
@@ -14,48 +13,50 @@ xacro_file = get_file_path(
 )
 robot_description = get_robot_description(xacro_file)
 
-# gazebo:
-gazebo = IncludeLaunchDescription(
-    get_file_path("ros_gz_sim", ["launch"], "gz_sim.launch.py"),
-    launch_arguments={
-        "gz_args": "empty.sdf -r",
-    }.items(),
-)
-
-# robot_state_publisher:
 robot_state_publisher = Node(
     package="robot_state_publisher",
     executable="robot_state_publisher",
-    name="robot_state_publisher",
-    output="both",
     parameters=[robot_description],
 )
 
-# spawn robot:
-spawn_robot = Node(
-    package="ros_gz_sim",
-    executable="create",
-    arguments=["-topic", "/robot_description"],
-    output="screen",
+gazebo_robot = IncludeLaunchDescription(
+    get_file_path("rcdt_utilities", ["launch"], "gazebo_robot.launch.py")
+)
+
+fr3_arm_controller_config = get_file_path(
+    "rcdt_franka", ["config"], "simulation_controllers.yaml"
+)
+fr3_arm_controller = Node(
+    package="controller_manager",
+    executable="spawner",
+    arguments=["fr3_arm_controller", "-p", fr3_arm_controller_config],
+)
+
+rviz = IncludeLaunchDescription(
+    get_file_path("rcdt_utilities", ["launch"], "rviz.launch.py"),
+    launch_arguments={"rviz_frame": "base_link"}.items(),
+)
+
+joint_state_broadcaster = Node(
+    package="controller_manager",
+    executable="spawner",
+    arguments=[
+        "joint_state_broadcaster",
+        "-t",
+        "joint_state_broadcaster/JointStateBroadcaster",
+    ],
+)
+
+moveit = IncludeLaunchDescription(
+    get_file_path("rcdt_franka", ["launch"], "moveit.launch.py"),
+    launch_arguments={
+        "moveit": "servo",
+    }.items(),
 )
 
 # get controller parameters:
-controller_parameters = get_file_path(
+controllers_config = get_file_path(
     "rcdt_mobile_manipulator", ["config"], "ros_controller.yaml"
-)
-
-# joint_trajectory_controller:
-joint_trajectory_controller = Node(
-    package="controller_manager",
-    executable="spawner",
-    name="joint_trajectory_controller",
-    arguments=[
-        "joint_trajectory_controller",
-        "-t",
-        "joint_trajectory_controller/JointTrajectoryController",
-        "-p",
-        controller_parameters,
-    ],
 )
 
 # diff drive controller:
@@ -68,27 +69,19 @@ diff_drive_controller = Node(
         "-t",
         "diff_drive_controller/DiffDriveController",
         "-p",
-        controller_parameters,
+        controllers_config,
     ],
-)
-
-# clock sync, otherwise diff_drive_conroller doesn't work:
-clock_sync = Node(
-    package="ros_gz_bridge",
-    executable="parameter_bridge",
-    name="panther_base_gz_bridge",
-    arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"],
 )
 
 
 def generate_launch_description() -> None:
     return LaunchDescription(
         [
-            gazebo,
             robot_state_publisher,
-            spawn_robot,
-            joint_trajectory_controller,
-            diff_drive_controller,
-            clock_sync,
+            gazebo_robot,
+            joint_state_broadcaster,
+            fr3_arm_controller,
+            moveit,
+            rviz,
         ]
     )
